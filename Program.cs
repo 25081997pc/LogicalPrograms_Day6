55cccc90cbf61a2c77aa63e3c55af8a532cc749f
25081997pc <chavanpooja2508@gmail.com>
2022-05-28 17:10:14 +0530

    Adding LogicalProgram

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReverseNumber.PrintTheReverseNumber();
+
+        }
+    }
+}
